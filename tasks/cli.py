3fdfc108196792a2e4e49ebc8a885a464a760d89
jjorissen52 <jjorissen52@gmail.com>
2020-05-16 01:34:39 -0500

    list, create, and delete cloud tasks from cli

--- tasks/cli.py
+++ tasks/cli.py
@@ -1,21 +1,134 @@
-import functools
 import os
 import sys
 import json
 import django
 
+import functools
+import subprocess
+
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 django.setup()
 
-import tasks.models as models
 from django.contrib.auth import get_user_model
+
+import tasks.models as models
+from tasks import cloud_tasks, conf
 from tasks.openid import create_token, decode_token
 
 User = get_user_model()
 
 
-def output(func):
+def list_steps(offset=0, limit=100):
+    return list(models.Step.objects
+                .values("name", "action", "id", "payload", "success_pattern", "task_id", "task__name")[offset:limit])
+
+
+def execute_step(name: str):
+    return models.Step.objects.get(name=name).execute()
+
+
+def list_tasks(offset=0, limit=100):
+    return list(models.Task.objects.values('name')[offset:limit])
+
+
+def execute_task(name: str):
+    return models.Task.objects.get(name=name).execute()
+
+
+def list_clocks(offset=0, limit=100):
+    return list(models.Clock.objects.values(
+        'name',
+        'cron',
+        'management',
+        'status'
+    )[offset:limit])
+
+
+def tick_clock(name: str):
+    return models.Clock.objects.get(name=name).tick()
+
+
+def start_clock(name: str):
+    _, message = models.Clock.objects.get(name=name).start_clock()
+    return message
+
+
+def pause_clock(name: str):
+    _, message = models.Clock.objects.get(name=name).pause_clock()
+    return message
+
+
+def delete_clock(name: str):
+    _, message = models.Clock.objects.get(name=name).delete_clock()
+    return message
+
+
+def sync_clock(name: str):
+    _, message = models.Clock.objects.get(name=name).sync_clock()
+    return message
+
+
+def list_schedules(offset=0, limit=100):
+    return list(models.TaskSchedule.objects.values(
+        "id",
+        "name",
+        "enabled",
+        "clock__name",
+        "clock_id",
+        "clock__cron",
+        "task__name",
+        "task_id"
+    )[offset:limit])
+
+
+def list_executions(offset=0, limit=100):
+    return list(models.TaskExecution.objects.values(
+        "id",
+        "results",
+        "status",
+        "task__name",
+        "task_id",
+    )[offset:limit])
+
+
+def register_service_account(email: str):
+    try:
+        User.objects.get(email__iexact=email)
+        return f'User for service account {email} already exists.'
+    except User.DoesNotExist:
+        User.objects.create(email=email, username=email)
+        return f'Created User for service account {email}.'
+
+
+def delete_service_account(email: str):
+    try:
+        User.objects.filter(email__iexact=email).delete()
+        return f'Deleted User for service account {email}'
+    except User.DoesNotExist:
+       return f'User for service account {email} does not exist.'
+
+
+def grant_required_roles(email: str):
+    cmd_template = "gcloud projects add-iam-policy-binding {PROJECT_ID} --member serviceAccount:{email} --role {role}"
+    roles = [
+        'roles/cloudtasks.admin',
+        'roles/cloudscheduler.admin',
+        'roles/cloudfunctions.invoker',
+    ]
+    for role in roles:
+        cmd = cmd_template.format(PROJECT_ID=conf.PROJECT_ID, email=email, role=role)
+        process = subprocess.Popen(cmd.split(), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        stdout, stderr = process.communicate()
+        if stdout:
+            sys.stdout.write(f'{stdout.decode()}\n')
+        if stderr:
+            sys.stderr.write(f'{stderr.decode()}\n')
+        sys.stdout.flush()
+        sys.stderr.flush()
+
+
+def __output(func):
     @functools.wraps(func)
     def inner(*args, **kwargs):
         result = func(*args, **kwargs)
@@ -30,114 +143,19 @@ def output(func):
     return inner
 
 
-@output
-def list_steps(offset=0, limit=100):
-    return list(models.Step.objects
-                .values("name", "action", "id", "payload", "success_pattern", "task_id", "task__name")[offset:limit])
-
-
-@output
-def execute_step(name: str):
-    return models.Step.objects.get(name=name).execute()
-
-
-@output
-def list_tasks(offset=0, limit=100):
-    return list(models.Task.objects.values('name')[offset:limit])
-
-
-@output
-def execute_task(name: str):
-    return models.Task.objects.get(name=name).execute()
-
-
-@output
-def list_clocks(offset=0, limit=100):
-    return list(models.Clock.objects.values(
-        'name',
-        'cron',
-        'management',
-        'status'
-    )[offset:limit])
-
-
-@output
-def tick_clock(name: str):
-    return models.Clock.objects.get(name=name).tick()
-
-
-@output
-def start_clock(name: str):
-    _, message = models.Clock.objects.get(name=name).start_clock()
-    return message
-
-
-@output
-def pause_clock(name: str):
-    _, message = models.Clock.objects.get(name=name).pause_clock()
-    return message
-
-
-@output
-def delete_clock(name: str):
-    _, message = models.Clock.objects.get(name=name).delete_clock()
-    return message
-
-
-@output
-def sync_clock(name: str):
-    _, message = models.Clock.objects.get(name=name).sync_clock()
-    return message
-
-
-@output
-def list_schedules(offset=0, limit=100):
-    return list(models.TaskSchedule.objects.values(
-        "id",
-        "name",
-        "enabled",
-        "clock__name",
-        "clock_id",
-        "clock__cron",
-        "task__name",
-        "task_id"
-    )[offset:limit])
-
-
-@output
-def list_executions(offset=0, limit=100):
-    return list(models.TaskExecution.objects.values(
-        "id",
-        "results",
-        "status",
-        "task__name",
-        "task_id",
-    )[offset:limit])
-
-
-@output
-def register_service_account(email: str):
-    try:
-        User.objects.get(email__iexact=email)
-        return f'User for service account {email} already exists.'
-    except User.DoesNotExist:
-        User.objects.create(email=email, username=email)
-        return f'Created User for service account {email}.'
-
-
-@output
-def delete_service_account(email: str):
-    try:
-        User.objects.filter(email__iexact=email).delete()
-        return f'Deleted User for service account {email}'
-    except User.DoesNotExist:
-       return f'User for service account {email} does not exist.'
+def __apply_output(current_dict):
+    for key, value in current_dict.items():
+        if callable(value):
+            # works because dicts are mutable
+            current_dict[key] = __output(value)
+        else:
+            __apply_output(value)
 
 
 if __name__ == '__main__':
     import fire
 
-    fire.Fire({
+    exec_map = {
         'tasks': {
             'list': list_tasks,
             'exec': execute_task,
@@ -163,13 +181,25 @@ if __name__ == '__main__':
         'auth': {
             'open_id': {
                 'token': {
-                    'create': output(create_token),
-                    'decode': output(decode_token),
+                    'create': create_token,
+                    'decode': decode_token,
                 }
             },
             'service_account': {
                 'register': register_service_account,
                 'delete': delete_service_account,
             },
-        }
-    })
+        },
+        'cloud': {
+            'service_account': {
+                'grant_required_roles': grant_required_roles,
+            },
+            'tasks': {
+                'list': cloud_tasks.list_tasks,
+                'create': cloud_tasks.create_task,
+                'delete': cloud_tasks.delete_task,
+            }
+        },
+    }
+    __apply_output(exec_map)
+    fire.Fire(exec_map)
---
+++ tasks/cloud_tasks.py
@@ -0,0 +1,63 @@
+"""
+Some wrapper methods around Google Cloud Tasks
+ref: https://googleapis.dev/python/cloudtasks/latest/gapic/v2/api.html
+"""
+import json
+import datetime
+from typing import Optional, Union, List
+
+from google.cloud import tasks_v2
+from google.cloud.tasks_v2.proto.task_pb2 import Task
+from google.protobuf import timestamp_pb2
+
+from tasks.conf import PROJECT_ID, REGION, SERVICE_ACCOUNT
+
+
+def list_tasks(queue: str) -> List[Task]:
+    client = tasks_v2.CloudTasksClient()
+    full_queue_name = client.queue_path(PROJECT_ID, REGION, queue)
+    attributes = ['url', 'http_method', 'headers', 'oidc_token']
+
+    return [{'name': task.name, **{attr: getattr(task.http_request, attr) for attr in attributes}}
+            for task in client.list_tasks(full_queue_name)]
+
+
+def create_task(
+        queue: str,
+        url: str,
+        payload: Optional[Union[str, dict, list, tuple]] = None,
+        service_account: str = SERVICE_ACCOUNT,
+        delay: int = 0
+) -> Task:
+    client = tasks_v2.CloudTasksClient()
+    full_queue_name = client.queue_path(PROJECT_ID, REGION, queue)
+    task = {
+        'http_request': {  # Specify the type of request.
+            'http_method': 'POST',
+            'url': url,  # The full url path that the task will be sent to.
+            'oidc_token': {
+                'service_account_email': service_account,
+            }
+
+        }
+    }
+    if isinstance(payload, str):
+        task['http_request']['body'] = payload
+    elif isinstance(payload, (dict, list, tuple)):
+        task['http_request']['body'] = json.dumps(payload)
+
+    scheduled_time = datetime.datetime.utcnow() + datetime.timedelta(seconds=delay)
+    timestamp = timestamp_pb2.Timestamp()
+    timestamp.FromDatetime(scheduled_time)
+
+    task['schedule_time'] = timestamp
+    return client.create_task(full_queue_name, task)
+
+
+def delete_task(queue: str, name: str):
+    client = tasks_v2.CloudTasksClient()
+    full_queue_name = client.queue_path(PROJECT_ID, REGION, queue)
+    if name.startswith(f'{full_queue_name}/tasks/'):
+        name = name.split(f'{full_queue_name}/tasks/')[-1]
+    full_task_name = client.task_path(PROJECT_ID, REGION, queue, name)
+    return client.delete_task(full_task_name)
